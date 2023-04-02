ddd4d0149719c7848b6fe807b79b68c6fea9d72c
Daniel Burgos <dburgos.developer@gmail.com>
2023-04-02 14:52:42 -0500

    Plataforma de pagos funcional

--- api/src/controllers/CreatePaymentController.js
+++ api/src/controllers/CreatePaymentController.js
@@ -1,24 +1,22 @@
 const request = require('request');
+const { createDonation } = require("../handlers/CreateDonationHandler")
 
 const { CLIENT,SECRET } = process.env;
-
 
 const PAYPAL_API = 'https://api-m.sandbox.paypal.com'; // Live https://api-m.paypal.com
 
 const auth = { user: CLIENT, pass: SECRET }
 
-let userId = null;
+
+
+
 
 const createPayment = async (req, res) => {
 
-    let userId = req.user.id;
-    console.log(userId);
-
+    const userId = req.user.id;
     const {amount, projectId} = req.body;
-    console.log(amount);
-    console.log(projectId);
-
     const {token} = req.query
+
     if (token !== undefined){
         res.status(201).json("Salio bien o mal")
     }
@@ -27,14 +25,14 @@ const createPayment = async (req, res) => {
         purchase_units: [{
             amount: {
                 currency_code: 'USD', //https://developer.paypal.com/docs/api/reference/currency-codes/
-                value: '5'
+                value: amount
             }
         }],
         application_context: {
             brand_name: `Linking future`,
             landing_page: 'NO_PREFERENCE', // Default, para mas informacion https://developer.paypal.com/docs/api/orders/v2/#definition-order_application_context
             user_action: 'PAY_NOW', // Accion para que en paypal muestre el monto del pago
-            return_url: `http://localhost:3001/execute-payment`, // Url despues de realizar el pago
+            return_url: `http://localhost:3001/execute-payment?userId=${userId}&projectId=${projectId}&amount=${amount}`, // Url despues de realizar el pago
             cancel_url: `http://localhost:3001/cancel-payment` // Url despues de realizar el pago
         }
     }
@@ -52,16 +50,33 @@ const createPayment = async (req, res) => {
 }
 
 const executePayment = async (req, res) => {
-    const {token} = req.query
+    const {token, userId, projectId, amount} = req.query
 
     request.post(`${PAYPAL_API}/v2/checkout/orders/${token}/capture`, {
         auth,
         body: {},
         json: true
     }, (err, response) => {
-        res.json({ data: response.body })
+        if(err){
+            res.status(400).json({ error: 'Hay un error' });
+        }
+        else{
+            const { status} = response.body;
+            if (status === 'COMPLETED') {
+                console.log(response.body);
+                console.log(amount);
+                console.log(userId);
+                console.log(projectId);
+
+                createDonation(amount,userId,projectId);
+
+                res.status(201).json("Se realizo el pago correctamente")
+            } else {
+                res.status(400).json({ error: 'La transacción no se ha completado exitosamente' });
+            }
+        }
     })
-    res.status(201).json("Se realizo el pago correctamente")
+    
 }
 
 const cancelPayment = async (req, res) => {
