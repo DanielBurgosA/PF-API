c3ea188f163192f7d9c0f55f7a8d3209acf20312
AlanSilcher <silcheralan@gmail.com>
2023-04-03 20:47:18 -0300

    Redireccion pago

--- api/src/controllers/CreatePaymentController.js
+++ api/src/controllers/CreatePaymentController.js
@@ -8,9 +8,6 @@ const PAYPAL_API = 'https://api-m.sandbox.paypal.com'; // Live https://api-m.pay
 const auth = { user: CLIENT, pass: SECRET }
 
 
-
-
-
 const createPayment = async (req, res) => {
 
     const userId = req.user.id;
@@ -33,7 +30,7 @@ const createPayment = async (req, res) => {
             landing_page: 'NO_PREFERENCE', // Default, para mas informacion https://developer.paypal.com/docs/api/orders/v2/#definition-order_application_context
             user_action: 'PAY_NOW', // Accion para que en paypal muestre el monto del pago
             return_url: `http://localhost:3001/execute-payment?userId=${userId}&projectId=${projectId}&amount=${amount}`, // Url despues de realizar el pago
-            cancel_url: `http://localhost:3001/cancel-payment` // Url despues de realizar el pago
+            cancel_url: `http://localhost:3000/cancel-payment` // Url despues de realizar el pago
         }
     }
     //https://api-m.sandbox.paypal.com/v2/checkout/orders [POST]
@@ -66,7 +63,7 @@ const executePayment = async (req, res) => {
 
                 createDonation(amount,userId,projectId);
 
-                res.status(201).json("Se realizo el pago correctamente")
+                res.redirect("http://localhost:3000/execute-payment");
             } else {
                 res.status(400).json({ error: 'La transacción no se ha completado exitosamente' });
             }
@@ -75,9 +72,5 @@ const executePayment = async (req, res) => {
     
 }
 
-const cancelPayment = async (req, res) => {
-    res.status(201).json("Se cancelo el pago correctamente")
-  
-}
 
-module.exports = {createPayment, executePayment, cancelPayment};
+module.exports = {createPayment, executePayment };
