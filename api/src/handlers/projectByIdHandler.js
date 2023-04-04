a845445bde8bf63b822d67fbe15437df404f337a
AlanSilcher <silcheralan@gmail.com>
2023-04-04 01:15:09 -0300

    agrego la busqueda de proyectos por id

---
+++ api/src/handlers/projectByIdHandler.js
@@ -0,0 +1,8 @@
+const { Project } = require("../db");
+
+const projectByIdHandler = async (id) => {
+  const searchProject = await Project.findOne({ where: { id: id } });
+  return searchProject;
+};
+
+module.exports = { projectByIdHandler };
