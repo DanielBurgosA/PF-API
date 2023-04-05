5f47c40d6b92210ff1b5c41ae50a7ce030181ea2
Javier Vasquez <vp.javier25@gmail.com>
2023-04-05 04:00:20 -0500

    think that is the last commit

--- api/src/handlers/LogInHandler.js
+++ api/src/handlers/LogInHandler.js
@@ -6,31 +6,30 @@ async function LogIn(user_email, user_password) {
 
     const user = await User.findOne({ where: { user_email: user_email } });
 
+
+if (!user) {
+    return { success: false, msg: "user not found" };
+} else if (user.deleted) {
+    return { success: false, msg: "user banned" };
+}
+
+const isValid = validPassword(user_password, user.hash, user.salt);
+
+if (isValid) {
+    const tokenObject = issueJWT(user);
+
     const userFix = {
         name: user.user_name,
         lastname: user.user_lastname,
         email: user.user_email,
         image: user.user_image
-
     }
 
+    return { token: tokenObject.token, success: true, msg: "you are in", origin: "local", rol: user.admin, userFix }
 
-    if (!user) {
-        return { success: false, msg: "user not found" };
-    } else if (user.deleted) {
-        return { success: false, msg: "user banned" };
-    }
-
-    const isValid = validPassword(user_password, user.hash, user.salt);
-
-    if (isValid) {
-        const tokenObject = issueJWT(user);
-
-        return { token: tokenObject.token, success: true, msg: "you are in", origin: "local", rol: user.admin, userFix }
-
-    } else {
-        return { success: false, msg: "you entered the wrong password" };
-    }
+} else {
+    return { success: false, msg: "you entered the wrong password" };
+}
 }
 
 module.exports = { LogIn };
